b4fb1d7ee3df0bc79efdf8feb4f2456de6ff1579
RailsTutorialBeginner <kydmonly@gmail.com>
2019-12-18 13:06:00 +0900

    Finish student signup

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,4 +1,25 @@
 class StudentsController < ApplicationController
   def new
+    @student = Student.new
   end
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def create
+    @student = Student.new(student_params)
+    if @student.save
+      flash[:success] = "Welcome to Labo Reach!"
+      redirect_to @student
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def student_params
+      params.require(:student).permit(:name, :email, :password, :password_confirmation)
+    end
 end
